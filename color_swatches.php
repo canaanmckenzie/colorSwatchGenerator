9d5dae52a1a5453fde4a6a1853830312aa366ad9
Canaan <cjmckenz@uvm.edu>
2024-02-08 22:35:02 -0500

    builds color schemes between range of values

---
+++ color_swatches.php
@@ -0,0 +1,73 @@
+<?php
+// Function to generate a gradient of hex codes between two colors
+function generateGradient($startColor, $endColor, $steps) {
+    $startRGB = sscanf($startColor, "%02x%02x%02x");
+    $endRGB = sscanf($endColor, "%02x%02x%02x");
+    
+    $gradient = array();
+
+    for ($i = 0; $i <= $steps; $i++) {
+        $r = $startRGB[0] + ($endRGB[0] - $startRGB[0]) * $i / $steps;
+        $g = $startRGB[1] + ($endRGB[1] - $startRGB[1]) * $i / $steps;
+        $b = $startRGB[2] + ($endRGB[2] - $startRGB[2]) * $i / $steps;
+        $gradient[] = sprintf("#%02x%02x%02x", $r, $g, $b);
+    }
+    return $gradient;
+}
+
+$gradients = array(
+    array("df1f00", "00ff00"),
+    array("0000ff", "ffff00"),
+    array("000000", "ff00ff"),
+    array("bdbdbd", "15bb84"),
+);
+
+$steps = 20; // Number of steps in the gradient
+
+
+//generate html
+$htmlContent = '<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width", initial-scale=1.0">
+<title>color</title>
+<style>
+body { background-color: #070d0d; color: #232323; }
+h2 { color: #fff }
+.gradient-container { display: flex; flex-direction: column; }
+.gradient-row { display: flex; flex-direction: row; }
+.gradient-box { width: 80px; height: 80px; margin: 2px; }
+</style>
+</head>
+<body>';
+
+foreach ($gradients as $gradientSet) {
+    $startColor = $gradientSet[0];
+    $endColor = $gradientSet[1];
+    $gradient = generateGradient($startColor, $endColor, $steps);
+    $htmlContent .= '<div class="gradient-container">';
+    $htmlContent .= '<h2>'.$startColor.'-'.$endColor.'</h2>';
+    $htmlContent .= generateGradientHTML($gradient);
+    $htmlContent .= '</div>';           
+}
+
+$htmlContent .= '</body></html>';
+
+function generateGradientHTML($gradient){
+    $html = '<div class="gradient-row">';
+    foreach($gradient as $color) {
+        $html .= 
+              '<div class="gradient-box" style="background-color:'
+              .$color.';">'.$color.'</div>';
+    }
+    $html .= '</div>';
+    return $html;
+}
+
+//write html contents 
+file_put_contents('index.html',$htmlContent);
+//print to terminal
+echo $htmlContent
+
+?>
